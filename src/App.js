b31711c3c8f33022026cc4d7a72be96c3716c294
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2022-10-16 23:17:43 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+// Since setState is async, you should declare a function explicitly to pass into setState. Otherwise, we can't test it sync since it is async. Also you should export those functions to import into the test file. That is the isolation process for real unit test.
+export const doDecrement = (prevState) => {
+	return prevState - 1;
+};
+
+export const doIncrement = (prevState) => {
+	return prevState + 1;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	const style = {
+		border: '3px solid black',
+		margin: '10px',
+		width: '30px',
+		fontSize: '24px',
+		borderRadius: '5px',
+		padding: '2px',
+	};
+
+	const onDecrement = () => {
+		setCount(doDecrement);
+	};
+	const onIncrement = () => {
+		setCount(doIncrement);
+	};
+
+	return (
+		<div className='App'>
+			<div style={style}>{count}</div>
+			<div>
+				<button onClick={onIncrement}>Increment</button>
+				<button onClick={onDecrement}>Decrement</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
