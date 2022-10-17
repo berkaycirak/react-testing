bd585991ed35261ff5ef612467db51a2d22706c9
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2022-10-17 22:38:40 +0300

    CRUD is done on server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
 import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -22,10 +23,47 @@ const data = [
 		surname: 'Russel',
 	},
 ];
+
 // json helps to make better visual appearance like indented curly brackets or etc.
 app.use(json());
+app.use(bodyParser());
 
+// Read
 router.get('/', (ctx) => (ctx.body = data));
+// Add
+router.post('/add', (ctx) => {
+	const userInput = ctx.request.body;
+
+	ctx.body = userInput;
+});
+// Update
+router.put('/update', (ctx) => {
+	const userInput = ctx.request.body;
+	const index = data.findIndex((item) => item.id === userInput.id);
+	let message;
+	if (index === -1) {
+		data.push(userInput);
+		message = 'Data added';
+	} else {
+		data[index] = userInput;
+		message = 'Data updated.';
+	}
+	ctx.body = message;
+});
+
+// Delete
+router.delete('/delete', (ctx) => {
+	const userInput = ctx.request.body;
+	const index = data.findIndex((item) => item.id === userInput.id);
+	let message;
+	if (index === -1) {
+		message = 'Data not found';
+	} else {
+		delete data[index];
+		message = 'Data deleted';
+	}
+	ctx.body = message;
+});
 
 // Router Middleware
 app.use(router.routes()).use(router.allowedMethods());
