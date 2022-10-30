b2301c6bd9861fc8c65a343dc669befed7967931
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2022-10-30 19:44:30 +0300

    Testing

--- jest-testingLibrary/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('should render', () => {
-	render(<App />);
-	const titleEl = screen.getByTestId('title');
-	expect(titleEl).toBeInTheDocument();
-});
